e9090adca29069c0fed2196fc6068707335446b7
Gerlof Fokkema <gerlof.fokkema@dataexpert.nl>
2025-08-29 00:31:58 +0200

    Remove gui code from rom testing.

--- src/main.zig
+++ src/main.zig
@@ -1,68 +1,8 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
 const Check = std.heap.Check;
-
-const GLFW = @import("glfw/glfw.zig");
-const Context = @import("gl/context.zig");
-
 const System = @import("system/system.zig");
-const CPU = System.CPU;
 const ROM = System.ROM;
-
-const vertices = [_]f32{ -0.5, -0.5, 0.0, 0.5, -0.5, 0.0, 0.0, 0.5, 0.0 };
-const indices = [_]u32{ 0, 1, 3, 1, 2, 3 };
-
-pub fn create_window(alloc: Allocator, system: *System) !void {
-    var glfw = try GLFW.init(alloc);
-    defer glfw.deinit();
-
-    var context = Context.init();
-    defer context.deinit();
-
-    var window = try glfw.window(alloc);
-    defer window.deinit();
-
-    const vao = Context.VAO.init();
-    defer vao.deinit();
-    const vbo = Context.VBO.vbo(.Array, f32).init();
-    defer vbo.deinit();
-
-    vao.attrib(f32, 0, 3, 3 * @sizeOf(f32), 0);
-    vbo.upload(&vertices);
-
-    try context.program(alloc, "res/shader.vs", "res/shader.fs");
-    var program = context.state.program.?;
-
-    while (!window.is_close()) {
-        while (glfw.next()) |e| {
-            std.debug.print("event: {any}\n", .{e});
-            switch (e) {
-                .err => {},
-                .frame => {},
-                .key_down => |k| switch (k) {
-                    .ESC, .Q => window.close(),
-                    .R => system.cpu.print(),
-                    else => std.log.debug("key `{}` not implemented yet\n", .{k}),
-                },
-                .key_repeat => {},
-                .key_up => {},
-                .mouse_down => {},
-                .mouse_up => {},
-            }
-        }
-
-        Context.clearColor(.{});
-        Context.clear();
-        program.use();
-        vao.bind();
-        Context.draw(.Triangles, @sizeOf(@TypeOf(vertices)));
-        vao.unbind();
-
-        window.render();
-        window.swap();
-        window.poll();
-    }
-}
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -72,16 +12,17 @@ pub fn main() !void {
     var system = try System.init(alloc);
     defer system.deinit(alloc);
 
-    var rom = try ROM.init(alloc, "lufia.sfc");
+    var rom = try ROM.init(alloc, "res/cpu_instrs.gb");
     defer rom.deinit();
-    try rom.check();
+    // try rom.check();
+    std.debug.print("{f}\n", .{rom.header()});
 
     std.debug.print("debug: {*}\n", .{system.memory.banks});
     std.debug.print("debug: {*}\n", .{system.cpu.memory.banks});
 
-    system.load(&rom);
-    for (0..20) |_| {
-        // if (i % 5 == 0) system.cpu.print();
-        system.cpu.step();
-    }
+    // system.load(&rom);
+    // for (0..20) |_| {
+    //     // if (i % 5 == 0) system.cpu.print();
+    //     system.cpu.step();
+    // }
 }
--- src/system/rom.zig
+++ src/system/rom.zig
@@ -1,63 +1,49 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
 
-const HDROFFS = 0x8000 - 0x40;
-const VECOFFS = 0x8000 - 0x20;
+const HDROFFS = 0x100;
 const MAXBUFF = 16 * 1024 * 1024;
 
-pub const Mode = enum(u8) {
-    lorom = 0x20,
-    hirom = 0x21,
-    sa1rom = 0x23,
-    fastlorom = 0x30,
-    fasthirom = 0x31,
-    sdd1rom = 0x32,
-    exhirom = 0x35,
-};
-
 const Header = extern struct {
-    const Vector = enum(u16) {
-        NM_COP = 0xFFE4,
-        NM_BRK = 0xFFE6,
-        NM_NMI = 0xFFEA,
-        NM_IRQ = 0xFFEE,
-        EM_COP = 0xFFF4,
-        EM_NMI = 0xFFFA,
-        EM_RST = 0xFFFC,
-        EM_IRQ = 0xFFFE,
-    };
-
-    title: [21]u8,
-    mode: Mode,
-    romtype: u8,
-    romsize: u8,
-    sramsize: u8,
-    devid: u8,
+    entrypoint: [4]u8,
+    logo: [48]u8,
+    title: [16]u8,
+    _1: [2]u8,
+    sgb: u8,
+    cartridge: u8,
+    rom_size: u8,
+    ram_size: u8,
+    country: u8,
+    _2: u8,
     version: u8,
-    chksumc: u16,
-    chksum: u16,
-    vectors: [16]u16,
+    chksum_header: u8,
+    chksum_global: [2]u8,
 
-    pub fn checksum(self: *Header, sum: u16) void {
-        self.chksumc = sum ^ 0xFFFF;
-        self.chksum = sum ^ 0x0000;
-    }
+    pub fn format(self: Header, writer: *std.io.Writer) std.io.Writer.Error!void {
+        const logo: [12]u32 = [_]u32{
+            0xceed6666,
+            0xcc0d000b,
+            0x03730083,
+            0x000c000d,
+            0x0008111f,
+            0x8889000e,
+            0xdccc6ee6,
+            0xddddd999,
+            0xbbbb6763,
+            0x6e0eeccc,
+            0xdddc999f,
+            0xbbb9333e,
+        };
 
-    pub fn print(self: *const Header) void {
-        std.debug.print("title:     {s}\n", .{self.title});
-        std.debug.print("mode:      {s}\n", .{@tagName(self.mode)});
-        std.debug.print("type:      {b:0>8}\n", .{self.romtype});
-        std.debug.print("rom size:  {d}\n", .{std.math.shl(u32, 0x400, self.romsize)});
-        std.debug.print("sram size: {d}\n", .{std.math.shl(u32, 0x400, self.sramsize)});
-        std.debug.print("devid:     0x{x:0>2}\n", .{self.devid});
-        std.debug.print("version:   0x{x:0>2}\n", .{self.version});
-        std.debug.print("chksumc:   0x{x:0>4}\n", .{std.mem.bigToNative(u16, self.chksumc)});
-        std.debug.print("chksum:    0x{x:0>4}\n", .{std.mem.bigToNative(u16, self.chksum)});
-        std.debug.print("vectors:\n", .{});
-        for (0.., self.vectors) |i, v| {
-            std.debug.print(" [0x{x:0>2}] 0x{x:0>4}\n", .{ VECOFFS + i * 2, v });
+        for (logo, 0..) |l, i| {
+            const lh = std.mem.readInt(u32, self.logo[i * 4 ..][0..4], .big);
+            std.debug.assert(l == lh);
         }
-        std.debug.print("\n", .{});
+        try writer.print(
+            \\Header {{
+            \\  .entrypoint = {x}, .title = "{s}", .cartridge = {x}
+            \\}}
+        , .{ self.entrypoint, self.title, self.cartridge });
     }
 };
 
@@ -71,32 +57,10 @@ pub fn init(alloc: Allocator, rom: []const u8) !ROM {
     return ROM{ .alloc = alloc, .data = data };
 }
 
-pub fn clone(self: *ROM, alloc: Allocator) !ROM {
-    const data = try alloc.alloc(u8, self.data.len);
-    @memcpy(data, self.data);
-    return ROM{ .data = data };
-}
-
 pub fn deinit(self: *ROM) void {
     self.alloc.free(self.data);
 }
 
 pub fn header(self: *const ROM) *Header {
     return @ptrCast(@alignCast(self.data[HDROFFS .. HDROFFS + 0x40].ptr));
-}
-
-pub fn check(self: *const ROM) !void {
-    const h = self.header();
-    std.debug.assert(h.chksum == h.chksumc ^ 0xFFFF);
-
-    var chk: u16 = 0;
-    for (self.data) |elem| {
-        chk = @addWithOverflow(chk, elem)[0];
-    }
-
-    if (h.chksum != chk) return error.CRCError;
-}
-
-pub fn args(self: *const ROM, instr: usize, bytes: usize) []u8 {
-    return self.data[instr + 1 .. instr + bytes];
 }
